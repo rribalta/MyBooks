f29c38702b587241f19230bc59aff53dbf3e840f
Ramon <ramonrt24@gmail.com>
2018-10-08 08:15:23 +0200

    Crear la classe BookItem

---
+++ app/src/main/java/model/BookItem.java
@@ -0,0 +1,10 @@
+package model;
+
+public class BookItem {
+    int Identificador;
+    String Titulo;
+    String Autor;
+//    Date Fecha_publicacion;
+    String Descripcion;
+    String URL_imagen_portada;
+}
